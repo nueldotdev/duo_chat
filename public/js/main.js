e440bbae086b5e9b1f5e2c4db96ce159a0ded235
nueldotdev <walternuel02@gmail.com>
2024-05-01 00:21:18 +0100

    UI for chat input (textarea resize on input)

--- public/js/main.js
+++ public/js/main.js
@@ -57,7 +57,25 @@ retrieveUserInfo()
 
 // Declare elements
 const logoutBtn = document.getElementById('logout-btn');
+const chatTextarea = document.getElementById('chat-textarea');
 // Add event listener
 logoutBtn.addEventListener('click', logout)
+chatTextarea.addEventListener('input', function() {
+    autoExpand(this);
+})
 
 
+function autoExpand(textarea) {
+    // Reset textarea height to auto to properly calculate new height
+    textarea.style.height = '40px';
+    // Calculate the new height based on the scroll height of the textarea
+    textarea.style.height = textarea.scrollHeight + 'px';
+    
+    // Limit the height to the maximum height if it exceeds it
+    if (textarea.scrollHeight > parseInt(window.getComputedStyle(textarea).maxHeight)) {
+      textarea.style.height = window.getComputedStyle(textarea).maxHeight;
+      textarea.style.overflowY = 'auto'; // Enable vertical scrollbar
+    } else {
+      textarea.style.overflowY = 'hidden'; // Hide vertical scrollbar if not needed
+    }
+  }
